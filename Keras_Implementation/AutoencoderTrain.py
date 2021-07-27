c881b27ed911315ff8f5d742d43bac09e348f7dd
qlstn9150 <qlstn9150@gmail.com>
2021-07-27 16:16:00 +0900

    0727

--- Keras_Implementation/AutoencoderTrain.py
+++ Keras_Implementation/AutoencoderTrain.py
@@ -19,13 +19,12 @@ def normalize_pixels(train_data, test_data):
     return train_norm, test_norm
 x_train, x_test = normalize_pixels(trainX, testX)
 
-#compression_ratios = [0.06, 0.09, 0.17, 0.26, 0.34, 0.43, 0.49]
-#SNR = [1, 4, 7, 13, 19]
+
+#snr = [0, 10, 20]
 compression_ratios = [0.06, 0.17, 0.34, 0.49]
 
 for comp_ratio in compression_ratios:
     tf.keras.backend.clear_session()
     c = Calculate_filters(comp_ratio)
     print('---> System Will Train, Compression Ratio: '+str(comp_ratio)+'. <---')
-    #_ = TrainAutoEncoder(x_train, x_test, nb_epoch=750, comp_ratio=comp_ratio, batch_size=16, c=c, snr=10, saver_step=50)
-    _ = TrainAutoEncoder(x_train, x_test, nb_epoch=3, comp_ratio=comp_ratio, batch_size=8, c=c, snr=20, saver_step=1)
+    _ = TrainAutoEncoder(x_train, x_test, nb_epoch=750, comp_ratio=comp_ratio, batch_size=16, c=c, snr=10, saver_step=50)
--- Keras_Implementation/Autoencoder_Evaluation.py
+++ Keras_Implementation/Autoencoder_Evaluation.py
@@ -18,41 +18,20 @@ from skimage.metrics import peak_signal_noise_ratio
 from matplotlib import pyplot as plt
 import h5py
 
-#all_tensors = [tensor for op in tf.get_default_graph().get_operations() for tensor in op.values()]
-#all_tensors[100:200]
+
 (trainX, _), (testX, _) = cifar10.load_data()
 
 def normalize_pixels(train_data, test_data):
-    #convert integer values to float
-	train_norm = train_data.astype('float32')
-	test_norm = test_data.astype('float32')
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-	return train_norm, test_norm
+    train_norm = train_data.astype('float32')
+    test_norm = test_data.astype('float32')
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    return train_norm, test_norm
 
-#normalizing the training and test data
 x_train, x_test = normalize_pixels(trainX, testX)
-#compression_ratios = [0.06, 0.09, 0.17, 0.26, 0.34, 0.43, 0.49]
-#SNR_dB = [0, 10, 20]
-#snr = 20
 
-def EvaluateModel(x_test, compression_ratios, snr, mode='multiple'):
-    
-    if mode=='single':
-        tf.keras.backend.clear_session()
-        comp_ratio=compression_ratios
-        path = './checkpoints/CompRatio{0}_SNR{1}/Autoencoder.h5'.format(comp_ratio, snr)
-        autoencoder = load_model(path, custom_objects={'NormalizationNoise': NormalizationNoise})
-        K.set_value(autoencoder.get_layer('normalization_noise_1').snr_db, snr)
-        pred_images = autoencoder.predict(x_test)*255
-        pred_images = pred_images.astype('uint8')
-        ssim = structural_similarity(testX, pred_images, multichannel=True)
-        psnr = compare_psnr(testX, pred_images)
-        return pred_images, psnr, ssim
-   
 
-    elif mode=='multiple':  
+def comp_eval(x_test, compression_ratios, snr, mode='multiple'): 
         model_dic = {'SNR':[], 'Pred_Images':[], 'PSNR':[], 'SSIM':[]}
         model_dic['SNR'].append(snr)
         for comp_ratio in compression_ratios:
@@ -63,22 +42,20 @@ def EvaluateModel(x_test, compression_ratios, snr, mode='multiple'):
             pred_images = autoencoder.predict(x_test)*255
             pred_images = pred_images.astype('uint8')
             ssim = structural_similarity(testX, pred_images, multichannel=True)
-            #psnr = compare_psnr(testX, pred_images)
             psnr = peak_signal_noise_ratio(testX, pred_images)
             model_dic['Pred_Images'].append(pred_images)
             model_dic['PSNR'].append(psnr)
             model_dic['SSIM'].append(ssim)
-        return model_dic
-            
+        return model_dic     
 
-def plot_model(x_test, compression_ratios, snr_lst, title, x_lablel, y_label):
+def comp_plot(x_test, compression_ratios, snr_lst, title, x_lablel, y_label):
     markers = ["*", "s", "o", "X", "d", "v", "<", ">", "^", "P", "H", "|"]
     colors = ['#800080', '#FF00FF', '#000080', '#008080', '#00FFFF', '#008000', '#00FF00']
     history = []
     i=0
     for snr in snr_lst:
         print('\n----> Now Getting Data and Preparing Plot for SNR {0} dB <----'.format(snr))
-        model_dic = EvaluateModel(x_test, compression_ratios, snr, mode='multiple')
+        model_dic = comp_eval(x_test, compression_ratios, snr, mode='multiple')
         history.append(model_dic)
         label='Deep JSCC (SNR={0}dB)'.format(snr)
         plt.plot(compression_ratios, model_dic['PSNR'], ls = '--', c = colors[i], marker = markers[i], label=label)
@@ -88,22 +65,56 @@ def plot_model(x_test, compression_ratios, snr_lst, title, x_lablel, y_label):
         plt.ylabel(y_label)
         plt.grid(True)
     plt.ylim(10,35)
+    plt.legend()
     plt.show()
     return history
 
-    
-compression_ratios=[0.06, 0.09, 0.17]
-snr=10
-snr_lst=[10]
-history = plot_model(x_test, compression_ratios, snr_lst, title='AWGN Channel', x_lablel='k/n', y_label='PSNR (dB)')  
+def snr_eval(x_test, comp_ratio, snr_test, snr_train): 
+        model_dic = {'Train_snr':[snr_train], 'Test_snr':[], 'PSNR':[]}
+        for snr in snr_test:
+            tf.keras.backend.clear_session()
+            path = './checkpoints/CompRatio{0}_SNR{1}/Autoencoder.h5'.format(comp_ratio, snr_train)
+            autoencoder = load_model(path, custom_objects={'NormalizationNoise': NormalizationNoise})
+            K.set_value(autoencoder.get_layer('normalization_noise_1').snr_db, snr)
+            pred_images = autoencoder.predict(x_test)*255
+            pred_images = pred_images.astype('uint8')
+            psnr = peak_signal_noise_ratio(testX, pred_images)
+            model_dic['Test_snr'].append(snr)
+            model_dic['PSNR'].append(psnr)
+        return model_dic 
+        
+def snr_plot(x_test, compression_ratio, snr_train, title, x_lablel, y_label):
+    markers = ["*", "s", "o", "X", "d", "v", "<", ">", "^", "P", "H", "|"]
+    colors = ['#800080', '#FF00FF', '#000080', '#008080', '#00FFFF', '#008000', '#00FF00']
+    history = []
+    i=0
+    for snr in snr_train:
+        print('\n----> Now Getting Data and Preparing Plot for SNR {0} dB <----'.format(snr))
+        model_dic = snr_eval(x_test, compression_ratio, snr_test, snr)
+        history.append(model_dic)
+        label='Deep JSCC (SNR_train={0}dB)'.format(snr)
+        plt.plot(snr_test, model_dic['PSNR'], ls = '--', c = colors[i], marker = markers[i], label=label)
+        i += 1
+        plt.title(title)
+        plt.xlabel(x_lablel)
+        plt.ylabel(y_label)
+        plt.grid(True)
+    plt.ylim(0,25)
+    plt.legend()
+    plt.show()
+    return history
 
 
-'''
-model_dic = EvaluateModel(x_test, compression_ratios, snr, mode='multiple')
-markers = ["*", "s", "o", "X", "d", "v", "<", ">", "^", "P", "H", "|"]
-colors = ['#800080', '#FF00FF', '#000080', '#008080', '#00FFFF', '#008000', '#00FF00', '#808000', '#FF0000', '#800000', '#000000', '#000080']
-plt.plot(compression_ratios, model_dic['PSNR'], ls = '--', c = 'm', marker = 'o')
-'''
+
+compression_ratios = [0.06, 0.17, 0.34, 0.49]
+snr_train=[0, 10, 20]
+history = comp_plot(x_test, compression_ratios, snr_train, title='AWGN Channel', x_lablel='k/n', y_label='PSNR (dB)') 
+
+
+#snr_train=[1, 4, 7, 13, 19]
+snr_test=[2,4,7,10,13,16,18,22,25,27]
+comp_ratio = 0.06
+history2 = snr_plot(x_test, comp_ratio, snr_train, title='AWGN Channel', x_lablel='SNR_test (dB)', y_label='PSNR (dB)') 
 
 '''
 j=6          
@@ -120,7 +131,6 @@ preds['PSNR'].append('PSNR')
 preds['SSIM'].append('SSIM')
 preds['Epoch'].append('Epoch')
 preds['Image'].append(true_img)
-
 for epoch in range(50,epoch_range,epoch_step):
     tf.keras.backend.clear_session()
     path = './CKPT_ByEpochs/CompRatio_{0}SNR{1}/Autoencoder_Epoch_{2}.h5'.format(comp_ratio, snr, epoch)
@@ -152,4 +162,4 @@ for j in range((len(preds['Image'])+2)//plt_step):
     i=i+plt_step        
     fig.axes.get_xaxis().set_visible(False)
     fig.axes.get_yaxis().set_visible(False)
-'''
+    '''
