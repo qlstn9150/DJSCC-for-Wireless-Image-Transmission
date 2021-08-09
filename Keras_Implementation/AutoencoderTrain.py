01c304d641249fa747e1d9aaa789ae6225d860b5
qlstn <qlstn9150@gmail.com>
2021-08-09 10:33:32 +0900

    snr10,20 comp

--- Keras_Implementation/AutoencoderTrain.py
+++ Keras_Implementation/AutoencoderTrain.py
@@ -27,4 +27,4 @@ for comp_ratio in compression_ratios:
     tf.keras.backend.clear_session()
     c = Calculate_filters(comp_ratio)
     print('---> System Will Train, Compression Ratio: '+str(comp_ratio)+'. <---')
-    _ = TrainAutoEncoder(x_train, x_test, nb_epoch=750, comp_ratio=comp_ratio, batch_size=16, c=c, snr=10, saver_step=50)
+    _ = TrainAutoEncoder(x_train, x_test, nb_epoch=750, comp_ratio=comp_ratio, batch_size=16, c=c, snr=20, saver_step=50)
